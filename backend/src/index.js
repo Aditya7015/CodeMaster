6406554b31d7123d6a8aef8e237b6e218744267f
Aditya Tiwari <ta2438549@gmail.com>
2026-05-24 00:19:26 +0530

    Socket api correction

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://codemaster-peach.vercel.app"
+      "https://code-master-opal.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
