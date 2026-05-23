430e6b8e6a0f114428cfce941e236f527e71ebd9
Aditya Tiwari <ta2438549@gmail.com>
2026-05-24 00:24:30 +0530

    API Issue Correction

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,7 @@
 // utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://codmaster-backend.onrender.com", {
+const socket = io("https://codemaster-sfk0.onrender.com", {
   withCredentials: true
 });
 export default socket;
